0851aacb05b7417ecfbe2cd79f3617b74f9cc903
Yash <ygulhane135@gmil.com>
2025-01-02 15:27:36 +0530

    Login UI

---
+++ src/components/error.jsx
@@ -0,0 +1,10 @@
+
+const Error = ({message}) => {
+  return (
+    <span className="text-sm text-red-400 ">
+      {message}
+    </span>
+  );
+}
+
+export default Error;
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,88 @@
+import {Card,CardContent,CardDescription,CardFooter,CardHeader,CardTitle} from "@/components/ui/card"
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { BeatLoader } from "react-spinners";
+import Error from "./error";
+import { useState } from "react";
+import * as Yup from 'yup';
 
 const Login = () => {
+
+    const [errors, setErrors] = useState([]);
+    const [formData, setformData] = useState({
+        email:"",
+        password:""
+    });
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setformData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }))
+    }
+
+    const handleLogin = async() => {
+        setErrors([])
+        try {
+            const schema = Yup.object().shape({
+                email:Yup.string()
+                .email("Invalid Email")
+                .required("Email is Required"),
+                password:Yup.string()
+                .min(6,"Password must be at least 6 characters")
+                .required("Password is Required"),
+            });
+
+            await schema.validate(formData,{abortEarly:false})
+            // api call
+        } catch (e) {
+            const newErrors = {};
+
+            e?.inner?.forEach((err) => {
+                newErrors[err.path] = err.message;
+            });
+
+            setErrors(newErrors);
+        }
+    }
   return (
     <div>
-      Login
+    <Card>
+        <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+                to your account if you already have one
+            </CardDescription>
+                <Error message={"some error"}/>
+        </CardHeader>
+        <CardContent className="space-y-2">
+            <div className="space-y-1">
+                <Input 
+                name="email" 
+                type="email" 
+                placeholder="Enter Email" 
+                onChange={handleInputChange}
+                />
+                {errors.email && <Error message={errors.email} />}
+            </div>  
+            <div className="space-y-1">
+                <Input 
+                name="password" 
+                type="password" 
+                placeholder="Enter password" 
+                onChange={handleInputChange}
+                />
+                {errors.password && <Error message={errors.password} />}
+            </div>
+        </CardContent>
+        <CardFooter>
+            <Button onClick={handleLogin}>
+                {true? <BeatLoader size={10} color="#36d7b7"/>:"Login"}
+                
+            </Button>
+        </CardFooter>
+    </Card>
     </div>
   );
 }
